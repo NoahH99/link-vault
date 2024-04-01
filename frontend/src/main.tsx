c5469d609346d5ba1b29e8382b4f176036a4119a
Grady Beck <140992111+gradymbeck@users.noreply.github.com>
2024-04-01 15:51:31 -0400

    Edited main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from "./App.tsx";
-import Info from "./Front.tsx";
+import Info from "./Info.tsx";
 import {createBrowserRouter, 
         RouterProvider,
 } from "react-router-dom";

cc810fd2861598ebf7c564f84bd829d134839e92
unknown <kimm10@vcu.edu>
2024-04-02 20:20:12 -0400

    Switched authentication service from using Flask to FastAPI

--- authentication-service/app.py
+++ authentication-service/app.py
@@ -1,10 +1,40 @@
-from flask import Flask
+from fastapi import FastAPI
+from fastapi.security import OAuth2AuthorizationCodeBearer
+from starlette.requests import Request
+from authlib.integrations.starlette_client import OAuth
+from starlette.config import Config
 
-app = Flask(__name__)
 
-@app.route('/api/authentication/health')
-def health_check():
-    return { "status": 200 }
+config = Config('.env')
+app = FastAPI()
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+oauth = OAuth()
+oauth.register(
+    name='google',
+    client_id=config('googleClientID'),
+    client_secret=config('googleClientSecret'),
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    refresh_token_url=None,
+    refresh_token_params=None,
+    client_kwargs={'scope': 'openid profile email'},
+)
+
+oauth2_scheme = OAuth2AuthorizationCodeBearer(
+    authorizationUrl='https://accounts.google.com/o/oauth2/auth',
+    tokenUrl='https://accounts.google.com/o/oauth2/token',
+    scopes={"openid", "email", "profile"}
+)
+
+@app.get("/login")
+async def login(request: Request):
+    redirect_uri = request.url_for('auth', _external=True)
+    return await oauth.google.authorize_redirect(request, config('redirectURL'))
+
+@app.route("/auth")
+async def auth(request: Request):
+    token = await oauth.google.authorize_access_token(request)
+    user = await oauth.google.parse_id_token(request, token)
+    return user

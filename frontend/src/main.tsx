ba26ce9218653d2c7ea001f13894c35c147f31b9
NoahH99 <noah@noahhendrickson.com>
2024-04-03 14:33:32 -0400

    Added bootstrap

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,24 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from "./App.tsx";
-import Info from "./Info.tsx";
-import {createBrowserRouter, 
-        RouterProvider,
-} from "react-router-dom";
+import {createBrowserRouter, RouterProvider,} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App/>
-    },
-    {
-        path: "/info",
-        element: <Info/>
-    },
+    }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <RouterProvider router={router}/>
     </React.StrictMode>,
 )

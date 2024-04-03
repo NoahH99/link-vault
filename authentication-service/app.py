61a2124ea69df590afd403c1017ec112130ed507
unknown <kimm10@vcu.edu>
2024-04-02 21:25:56 -0400

    Updated parameters for redirectURL so that it isn't a static link

--- authentication-service/app.py
+++ authentication-service/app.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request, Body
 from fastapi.security import OAuth2AuthorizationCodeBearer
-from starlette.requests import Request
 from authlib.integrations.starlette_client import OAuth
 from starlette.config import Config
+from fastapi.responses import RedirectResponse, JSONResponse
 
 
 config = Config('.env')
@@ -29,12 +29,21 @@ oauth2_scheme = OAuth2AuthorizationCodeBearer(
 )
 
 @app.get("/login")
-async def login(request: Request):
-    redirect_uri = request.url_for('auth', _external=True)
-    return await oauth.google.authorize_redirect(request, config('redirectURL'))
+async def login(request: Request, body: dict = Body(...)):
+    redirectURL = body.get("redirectURL")
+    if not redirectURL:
+        raise HTTPException(status_code=400, detail="Redirect URL not provided")
+    oauth2RedirectURL = request.url_for('auth', _external=True) + f"?redirectURL={redirectURL}"
+    return await oauth.google.authorize_redirect(request, oauth2RedirectURL)
 
 @app.route("/auth")
 async def auth(request: Request):
-    token = await oauth.google.authorize_access_token(request)
-    user = await oauth.google.parse_id_token(request, token)
-    return user
+    redirectURL = request.query_params.get("redirectURL")
+    try: 
+        token = await oauth.google.authorize_access_token(request)
+        return RedirectResponse(url=f"{redirectURL} ?token={token['access_token']}")
+    except Exception as e:
+        return JSONResponse(
+            status_code=400,
+            content={"success": False, "message": "Error authenticating user", "error": str(e)}
+        )

cb64833632eada7b943f99d53d172dfbe0b8d82e
NoahH99 <noah@noahhendrickson.com>
2024-04-03 15:05:25 -0400

    Added structure for main page... need to implement the apis

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,47 @@
+import {useState} from "react";
+import {Link} from "react-router-dom";
+
 function App() {
+
+    const [apiData, setApiData] = useState<string | null>(null);
+
+    const handleLoginClick = async () => {
+        try {
+            setApiData('logged in')
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
     return (
         <>
-            <h1>Welcome</h1>
+            {apiData == null && (
+                <button
+                    onClick={handleLoginClick}
+                    type="button"
+                    className="btn btn-primary btn-lg">
+                    Login with Google
+                </button>
+            )}
+            {apiData != null && (
+                <>
+                    <h1>Logged In</h1>
+                    <Link to="/shorten">
+                        <button
+                            type="button"
+                            className="btn btn-primary btn-lg">
+                            Shorten URL
+                        </button>
+                    </Link>
+                    <Link to="/uploader">
+                        <button
+                            type="button"
+                            className="btn btn-primary btn-lg">
+                            Upload Files
+                        </button>
+                    </Link>
+                </>
+            )}
         </>
     )
 }

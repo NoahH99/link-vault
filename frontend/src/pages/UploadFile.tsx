f8a9df2388cff4dd1632dd3363cb23b2adedb76c
NoahH99 <noah@noahhendrickson.com>
2024-04-04 02:08:25 -0400

    Moved the code from Uploader.tsx to UploadFile.tsx. Also made sure there were no linting errors.

--- frontend/src/pages/UploadFile.tsx
+++ frontend/src/pages/UploadFile.tsx
@@ -1,6 +1,24 @@
+import {ChangeEvent, FormEvent, useState} from "react";
+
 function UploadFile() {
+    const [files, setFiles] = useState<FileList | null>(null)
+
+    const handleUploadFiles = (event: FormEvent<HTMLFormElement>) => {
+        console.log(event);
+        console.log(files);
+    }
+
+    const updateFiles = (event: ChangeEvent<HTMLInputElement>) => {
+        setFiles(event.target.files);
+    }
+
     return (
         <>
+            <div>Select Files to Upload</div>
+            <form onSubmit={handleUploadFiles}>
+                <input type="file" id="upload" onChange={updateFiles} multiple/>
+                <input type="submit"/>
+            </form>
         </>
     );
 }

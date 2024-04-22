e1b7fb7e5739fee44703d43d67a18dd490651d15
Mariner <57468305+Mariner10@users.noreply.github.com>
2024-04-22 17:17:05 -0400

    Added Flipswitch functionality for sprint 3

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from typing import List
 from fastapi import FastAPI, File, UploadFile
 import os
+import asyncio
+import json
+
 
 app = FastAPI()
+
+def update_json_file(stateDict,filename):
+    with open(f"JSON/{filename}.json", "w") as file:
+        json.dump(stateDict, file)
 
 
 @app.post("/api/file-storage/upload/{linkName}")
@@ -43,3 +50,49 @@ async def sendFile(linkName):
     delete_directory(path)
 
     return {"message": f"Successfuly deleted file link [ {linkName} ] from server."}
+
+@app.post('/api/flipswitch/{flipswitch}/{state}')
+async def switch(flipSwitch,state):
+
+    try:
+        state = int(state)
+    except Exception:
+        return {"Status": 406, "Issue": "/api/flipswitch/[flipswitch]/[state]: state must be a number 0-4"}
+
+
+    if os.path.exists("JSON/flipSwitches.json"):
+        with open("JSON/flipSwitches.json", "r") as file:
+            stateDict = json.load(file)
+    else:
+        stateDict = {}
+
+    title = flipSwitch
+    if title not in stateDict:
+        stateDict[title] = {'state': False}
+
+    flipSwitch_state = stateDict[title]['state']
+
+    match state:
+        case 0:
+            flipSwitch_state = False
+        case 1:
+            flipSwitch_state = True
+        case 2:
+            flipSwitch_state = not flipSwitch_state
+        case 3:
+            return stateDict[title]
+        case 4:
+            stateDict.pop(title, None)
+            update_json_file(stateDict,"flipSwitches")
+            return stateDict[title]
+        case _:
+            return {"Status": 406, "Issue": "/api/flipswitch/[flipswitch]/[state]: state must be a number 0-4"}
+
+    stateDict[title]['state'] = flipSwitch_state
+    update_json_file(stateDict,"flipSwitches")
+
+    for key in stateDict:
+        print(str(key) + ": " + str(stateDict[key]))
+    return {"State": flipSwitch_state}
+
+

d0d7a81af99444c3e78ae56ec4bb7cda9f4fab7f
Mariner <57468305+Mariner10@users.noreply.github.com>
2024-04-22 20:38:06 -0400

    Added Flipswitch Functionality

---
+++ file-storage-service/api/endpoints/flipswitch.py
@@ -0,0 +1,55 @@
+from fastapi import APIRouter
+import asyncio
+import json
+import os
+
+router = APIRouter()
+
+def update_json_file(stateDict,filename):
+    with open(f"JSON/{filename}.json", "w") as file:
+        json.dump(stateDict, file)
+
+
+@router.get("/{flipswitch}/{state}")
+async def switch(flipSwitch,state):
+
+    try:
+        state = int(state)
+    except Exception:
+        return {"Status": 406, "Issue": "/api/flipswitch/[flipswitch]/[state]: state must be a number 0-4"}
+
+
+    if os.path.exists("JSON/flipSwitches.json"):
+        with open("JSON/flipSwitches.json", "r") as file:
+            stateDict = json.load(file)
+    else:
+        stateDict = {}
+
+    title = flipSwitch
+    if title not in stateDict:
+        stateDict[title] = {'state': False}
+
+    flipSwitch_state = stateDict[title]['state']
+
+    match state:
+        case 0:
+            flipSwitch_state = False
+        case 1:
+            flipSwitch_state = True
+        case 2:
+            flipSwitch_state = not flipSwitch_state
+        case 3:
+            return stateDict[title]
+        case 4:
+            stateDict.pop(title, None)
+            update_json_file(stateDict,"flipSwitches")
+            return stateDict[title]
+        case _:
+            return {"Status": 406, "Issue": "/api/flipswitch/[flipswitch]/[state]: state must be a number 0-4"}
+
+    stateDict[title]['state'] = flipSwitch_state
+    update_json_file(stateDict,"flipSwitches")
+
+    for key in stateDict:
+        print(str(key) + ": " + str(stateDict[key]))
+    return {"State": flipSwitch_state}
--- file-storage-service/api/routes/main.py
+++ file-storage-service/api/routes/main.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 
-from api.endpoints import health, upload
+from api.endpoints import health, upload, flipswitch
 
 router = APIRouter()
 
-prefix = "/api/file-storage"
 
-router.include_router(health.router, prefix=prefix)
-router.include_router(upload.router, prefix=prefix)
+router.include_router(health.router, prefix="/api/file-storage")
+router.include_router(upload.router, prefix="/api/file-storage")
+router.include_router(flipswitch.router, prefix="/api/flipswitch")

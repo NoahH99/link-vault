1a4170191a853d4750adb3d4292769644fa63d10
NoahH99 <noah@noahhendrickson.com>
2024-04-25 13:39:25 -0400

    Removed unused files.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import {BrowserRouter, Route, Routes,} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./pages/Home.tsx";
